87fbcacc8601e169130852eb0e5862d48862ed36
DIR\antonio.molinuevo <antoniog81@gmail.com>
2022-02-23 17:28:37 +0100

    Adding dbz form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/listado/heroes.module';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { HeroesModule } from './heroes/listado/heroes.module';
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
