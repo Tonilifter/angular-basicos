12841803a548b6d1ac83b4652389e033891945e7
DIR\antonio.molinuevo <antoniog81@gmail.com>
2022-02-24 19:20:28 +0100

    Personajes en array y componente hijo

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   })
 export class MainPageComponent {
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 10000
+    }    
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
 
   agregar(){
-    console.log('Esta es una prueba')
+    if (this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+
+    console.log(this.nuevo)
+    this.personajes.push(this.nuevo);
+    this.nuevo= {
+      nombre:'',
+      poder: 0
+    }
   }
 
 }
